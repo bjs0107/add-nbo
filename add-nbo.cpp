9878695ad2970e2379c85794c8d0a5bef8c6dab8
bjs0107 <snakebjs0107@naver.com>
2022-07-26 05:26:31 -0700

    Add add-nbo modules

---
+++ add-nbo.cpp
@@ -0,0 +1,8 @@
+#include "add-nbo.h"
+
+uint32_t add_NBO(uint32_t A, uint32_t B) {
+  	
+	uint32_t result = A+B;
+
+	return result;
+}
---
+++ add-nbo.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+
+uint32_t add_NBO(uint32_t A, uint32_t B);
